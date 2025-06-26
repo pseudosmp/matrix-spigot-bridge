5b01492abc17a413d10693b8b6997eb2bb50c2a7
Ilamparithi Murali <ilamparithi.in@gmail.com>
2025-06-26 11:21:43 +0530

    reloads now restart topic updater

--- src/main/java/com/pseudosmp/msb/commands/ReloadConfig.java
+++ src/main/java/com/pseudosmp/msb/commands/ReloadConfig.java
@@ -8,9 +8,11 @@ import org.bukkit.command.CommandSender;
 import com.pseudosmp.tools.game.ConfigUtils;
 
 public class ReloadConfig implements CommandExecutor {
-    ConfigUtils config;
+    private final ConfigUtils config;
+    private final MatrixSpigotBridge plugin;
 
     public ReloadConfig(MatrixSpigotBridge plugin) {
+        this.plugin = plugin;
         this.config = MatrixSpigotBridge.config;
     }
 
@@ -20,6 +22,7 @@ public class ReloadConfig implements CommandExecutor {
         String prevPwd = config.getMatrixPassword();
         String prevHomeserver = config.matrixServer;
         String prevRoomID = config.matrixRoomId;
+        int prevTopicUpdateInterval = config.matrixTopicUpdateInterval;
 
         if (!config.load()) {
             sender.sendMessage("§e[MatrixSpigotBridge] §cFailed to reload configuration. Please check the console for errors.");
@@ -33,6 +36,11 @@ public class ReloadConfig implements CommandExecutor {
             sender.sendMessage("§e[MatrixSpigotBridge] §aMatrix credentials changed! §7Please run §a/msb restart §7to connect with the new credentials.");
             return true;
         }
+
+        if (config.matrixTopicUpdateInterval != prevTopicUpdateInterval) {
+            plugin.updateRoomTopicAsync(success -> {});
+        }
+
         sender.sendMessage("§e[MatrixSpigotBridge] §aConfiguration reloaded.");
         return true;
     }
